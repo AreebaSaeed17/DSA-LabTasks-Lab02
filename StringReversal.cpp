868fe0cf50a0eb065486fc2e02fb9b4a88f2c02e
Areeba Saeed <asaeed.bese25seecs@seecs.edu.pk>
2026-09-18 16:07:55 +0500

    Adding code for reversal of string

--- StringReversal.cpp
+++ StringReversal.cpp
@@ -11,25 +11,39 @@ void StringReversal(){
     string *Str = new string() ;
 
     // taking user input
-    cout << "Enter the string of your choice to reverse it.\n";
+    cout << "Enter the string of your choice to reverse it..\n";
+    // getting a full line input via getline
     getline(cin, *Str);
 
-    // getting size of the string that the user actually used
+    // getting size of the string
+    // since it is on heap it can be accessed using the ptr name so for getting length
+    // i had to use ->length()
     int size = Str->length();
+    
+    // logic for swap
+    // get the size and find the midpoint
+    // i need to exchange index 0 with index 9
+    // index 1 with 9 
+    // index 2 with 8 and so on
+    // this can be generalized as size-currentindex-1 = i mathematically
+    // Ive first dereferenced each element of the string using dereferencing operator  
+    // and then swapped the individual characters at the positions using the formula 
 
-    // running a loop to swap the characters of the string
-    //for(int i =  0; i<(size/2); i++){
-   //     swap(*Str[i], *Str[size-i-1] );
-    //}
+    for( int i = 0; i < size / 2; i++) {
+        swap((*Str)[i], (*Str) [size-i-1] );       
+    }
 
     // printing the reversed string
-    cout<<"Following is the reversed string: \n";
+    cout<<"\n Following is the reversed string: \n";
     cout<<*Str;
 
+    // freeing up memory
 }
 
 int main()
 {
+    // calling the function
     StringReversal();
+
 }
 
